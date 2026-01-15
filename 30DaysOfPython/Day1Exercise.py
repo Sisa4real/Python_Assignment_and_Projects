34f9e7afa8902b41947742de367f9f5581f736e4
yusufbugaje20 <yusufbugaje10@gmail.com>
2026-01-15 14:43:31 +0100

    Day 1 Exercise

---
+++ 30DaysOfPython/Day1Exercise.py
@@ -0,0 +1,44 @@
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+print('Yusuf Bugaje')
+print('Bugaje Family')
+print('Nigeria')
+print('I am enjoying 30 days of python')
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print('Yusuf Bugaje')
+print('Bugaje Family')
+print('Nigeria')
+
+integer = 10
+float = 5.49
+complex = 4 + 3j
+
+string = 'Hello World'
+boolean = 5 == 5
+list = [1, 2, 3, 4, 5]
+tuple = (('yusuf', 'maryam', 'muktar', 'aisha'))
+set = ({1.2, 3.4, 4.5, 6.7, 8.9})
+dictionary = ({'name': 'yusuf bugaje', 'age': 27, 'country': 'nigeria'})
+
+x1 = 2
+x2 = 10
+y1 = 3
+y2 = 8
+
+import math
+
+euclidian_distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+print(euclidian_distance)
+
+
